bd446ef48c9636d1c153dcae5a29149297b495dc
psy0120 <psyrnrepd@gmail.com>
2020-07-23 19:17:02 +0900

    child/Test.java 생성

---
+++ WEB0717_oracle/src/main/java/com/sewon/anyone/board/service/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.sewon.anyone.board.service;
+
+/**
+ * @author Sewonit
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hello");
+	}
+
+}
